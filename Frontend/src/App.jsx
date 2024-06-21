4f5792698e8f6a67f1654a8712bf1eecf8a84209
Nicholas Matias <113799954+NicholasMatias@users.noreply.github.com>
2024-06-21 10:36:01 -0700

    Added footer for each board page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,32 +6,34 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 function App() {
 
   return (
-      <Router>
-        <Routes>
-          <Route path='/' element={
-            <div>
-                  <header id='title_container'>
-                    <h1 id='title'>Kudos Board</h1>
-                  </header>
-                  <main>
-                    <KudosCard_List />
-                  </main>
-                  <footer id='footer_container'>
-                    <h3>CBN, Creations by Nick</h3>
-                  </footer>
-            </div>
-          } />
-          <Route path='/:id' element={
-            <div>
-              <header id="title_container">
-                <h1>Board</h1>
-              </header>
-              <CardsList />
-              
-            </div>
-          }/>
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path='/' element={
+          <div>
+            <header id='title_container'>
+              <h1 id='title'>Kudos Board</h1>
+            </header>
+            <main>
+              <KudosCard_List />
+            </main>
+            <footer id='footer_container'>
+              <h3>CBN, Creations by Nick</h3>
+            </footer>
+          </div>
+        } />
+        <Route path='/:id' element={
+          <div>
+            <header id="title_container">
+              <h1>Board</h1>
+            </header>
+            <CardsList />
+            <footer id='footer_container'>
+              <h3>CBN, Creations by Nick</h3>
+            </footer>
+          </div>
+        } />
+      </Routes>
+    </Router>
   )
 }
 
